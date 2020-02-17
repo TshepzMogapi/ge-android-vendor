aaaf7771ebfb1fbbc86ace1be568afdd9bfd8556
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-02-18 01:54:43 +0200

    add click listener

---
+++ app/src/main/java/com/example/ge_android_vendor/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ge_android_vendor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.tv_count);
+        Log.d(LOG_TAG, "Hello World");
+    }
+
+    public void count(View view) {
+
+        countUp();
+        Toast toast = Toast
+                .makeText(this, R.string.counting_toast_message, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+
+    public void countUp() {
+        mCount++;
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+}
